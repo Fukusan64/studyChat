1e5c7bb4fac60e6284d7160c538886494c6f727e
DESKTOP-ELASF9J\kohta <kuroneko9963@gmail.com>
2018-08-28 15:38:50 +0900

    initial

---
+++ public/kuroneko/js/main.js
@@ -0,0 +1,14 @@
+$(() => {
+  const socket = io.connect();
+  socket.on('connect', () => {
+    const loginDialog = $('#loginDialog').get(0);
+    loginDialog.showModal();
+
+    $('#closeButton').click(() => {
+      if ($('#userName').val() !== '') {
+        socket.emit('join', $('#userName').val());
+        loginDialog.close();
+      }
+    });
+  });
+});

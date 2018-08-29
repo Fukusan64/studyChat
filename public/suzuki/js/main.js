9bcf4e3fe94fcfa95dc63a471d5247946482d255
szknab <yuki1220atria@gmail.com>
2018-08-30 02:43:37 +0900

    ログに時間・数字を追加、Ctrl+Enterなどのショートカットの実装、スクロール機能の実装、デザインを掲示板風に変更

--- public/suzuki/js/main.js
+++ public/suzuki/js/main.js
@@ -1,7 +1,12 @@
 $(()=> {
   const socket = io.connect();
   socket.on('connect', () => {
+    const myId = socket.id;
     const loginDialog = $('#loginDialog').get(0);
+    let date;
+    let chatNum = 0;
+    let area = document.getElementById('messageArea');
+    let scrollHeight;
     loginDialog.showModal();
 
     $('#closeButton').click(() => {
@@ -11,10 +16,66 @@ $(()=> {
       }
     });
 
-    $('#sendButton').click(function(){
+    const buttomScroll = () => {
+      scrollHeight = area.scrollHeight;
+      area.scrollTop = scrollHeight;
+    }
+
+    if(area.scrollTop < scrollHeight){//if文の条件間違ってるよ
+      console.log('aaaaaa');
+      $('#messageArea')
+        .append(`<button id='downButton'>最後のログへ</button>`)
+      ;
+    };
+
+    $('#sendButton').click(() => {
       const message = $("#chatBar").val();
       if(message != '') {
         socket.emit('message', message);
+        $("#chatBar").val('');
+      }
+    });
+
+    socket.on('message', data => {
+      date = new Date;
+      chatNum += 1;
+      $('#messageArea')
+        .append(`<p style="color: ${data.id == myId ? 'green' : 'black'}">${chatNum}. ${data.userName} ${date.toLocaleString()}</br><span style="font-size: 30px; margin-left: 15px;">${data.message}</span></p><hr>`)
+      ;
+    });
+
+    socket.on('join', data => {
+      $('#messageArea')
+        .append(`<p style="color: lightgreen;">${data.userName}が入室しました。現在${data.numUsers}人。</p>`)
+      ;
+      buttomScroll();
+    });
+
+    socket.on('login', data => {
+      $('#messageArea')
+        .append(`<p style="color: lightgreen;">あなたが入室しました。現在${data.numUsers}人。</p>`)
+      ;
+      buttomScroll();
+    });
+
+    socket.on('user left', data => {
+      $('#messageArea')
+        .append(`<p style="color: lightgreen;">${data.userName}が退室しました。現在${data.numUsers}人。</p>`)
+      ;
+      buttomScroll();
+    });
+
+    $('#chatBar').keydown(e => {
+      if(event.ctrlKey){
+        if(e.keyCode === 13){
+          $('#sendButton').trigger('click');
+        }
+      }
+    });
+
+    $('#userName').keydown(e => {
+      if(e.keyCode === 13){
+        $('#closeButton').trigger('click');
       }
     });
   });

7547d953d9e0213a89b60193efcd14a80a87ddab
szknab <yuki1220atria@gmail.com>
2018-08-30 22:16:43 +0900

    デザインを現代風に大幅に変更、その他あるかも

--- public/suzuki/js/main.js
+++ public/suzuki/js/main.js
@@ -7,25 +7,38 @@ $(()=> {
     let chatNum = 0;
     let area = document.getElementById('messageArea');
     let scrollHeight;
+    let timer = false;
     loginDialog.showModal();
 
     $('#closeButton').click(() => {
       if($('#userName').val() !== '') {
         socket.emit('join', $('#userName').val());
         loginDialog.close();
+        $('#chatBar').focus();
       }
     });
 
-    const buttomScroll = () => {
+    $('#messageArea').scroll(() => {
+      if(timer !== false){
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        appearScrollButton();
+      }, 200);
+    });
+
+    const scrollBottom = () => {
       scrollHeight = area.scrollHeight;
       area.scrollTop = scrollHeight;
-    }
+    };
 
-    if(area.scrollTop < scrollHeight){//if文の条件間違ってるよ
-      console.log('aaaaaa');
-      $('#messageArea')
-        .append(`<button id='downButton'>最後のログへ</button>`)
-      ;
+    const appearScrollButton = () => {
+      if(area.scrollHeight - Math.round(area.scrollTop) > area.clientHeight){
+        $('#downButton').show();
+      }
+      else{
+        $('#downButton').hide();
+      }
     };
 
     $('#sendButton').click(() => {
@@ -40,29 +53,30 @@ $(()=> {
       date = new Date;
       chatNum += 1;
       $('#messageArea')
-        .append(`<p style="color: ${data.id == myId ? 'green' : 'black'}">${chatNum}. ${data.userName} ${date.toLocaleString()}</br><span style="font-size: 30px; margin-left: 15px;">${data.message}</span></p><hr>`)
+        .append(`<p style="color: ${data.id == myId ? '#a8da44' : '#444'}">${chatNum}. ${data.userName} ${date.toLocaleString()}</br><span style="font-size: 30px; margin-left: 15px;">${data.message}</span></p><hr>`)
       ;
+      scrollBottom();
     });
 
     socket.on('join', data => {
       $('#messageArea')
-        .append(`<p style="color: lightgreen;">${data.userName}が入室しました。現在${data.numUsers}人。</p>`)
+        .append(`<p style="color: #f1c550;">${data.userName}が入室しました。現在${data.numUsers}人。</p>`)
       ;
-      buttomScroll();
+      scrollBottom();
     });
 
     socket.on('login', data => {
       $('#messageArea')
-        .append(`<p style="color: lightgreen;">あなたが入室しました。現在${data.numUsers}人。</p>`)
+        .append(`<p style="color: #f1c550;">あなたが入室しました。現在${data.numUsers}人。</p>`)
       ;
-      buttomScroll();
+      scrollBottom();
     });
 
     socket.on('user left', data => {
       $('#messageArea')
-        .append(`<p style="color: lightgreen;">${data.userName}が退室しました。現在${data.numUsers}人。</p>`)
+        .append(`<p style="color: #f1c550;">${data.userName}が退室しました。現在${data.numUsers}人。</p>`)
       ;
-      buttomScroll();
+      scrollBottom();
     });
 
     $('#chatBar').keydown(e => {
@@ -78,5 +92,9 @@ $(()=> {
         $('#closeButton').trigger('click');
       }
     });
+
+    $('#downButton').click(() => {
+      scrollBottom();
+    })
   });
 });

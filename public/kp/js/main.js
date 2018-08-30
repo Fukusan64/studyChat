d6c8729f14938550be3d8ab43a70ed153f695227
kp54 <kangpang65@gmail.com>
2018-08-31 06:05:40 +0900

    オタクがあまりにも多用するので

--- public/kp/js/main.js
+++ public/kp/js/main.js
@@ -14,8 +14,9 @@ $(()=> {
 
     $('#sendButton').click(() => {
       var c = $('#chatBar');
-      if(c.val()  !== ''){
-        socket.emit('message', c.val());
+      var t = c.val();
+      if(t  !== ''){
+        socket.emit('message', t);
         c.val('');
       }
     });
@@ -23,6 +24,10 @@ $(()=> {
 
   socket.on('message', data => {
     var m = $('#messageArea');
+    if (data.message.includes(':thinking:'))
+    {
+      data.message = data.message.replace(':thinking:', '\u{1f914}');
+    }
     m.append(`<p>${data.userName} said: ${data.message}</p>`);
   })
 });
